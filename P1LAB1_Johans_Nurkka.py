d318ecc3510499a8c2668677513f0379a28c2583
JohansSolo <nurkkaj7276@student.faytechcc.edu>
2026-02-06 08:23:19 -0500

    Add files via upload

---
+++ P1LAB1_Johans_Nurkka.py
@@ -0,0 +1,9 @@
+# Johans Nurkka
+# 2/5/2026
+# P1LAB1
+# Prints a personalized message after entering requisite information
+
+first_name = input("Enter Your First Name:")
+last_name = input("Enter Your Last Name:")
+
+print("Hello,",first_name, last_name +"!","Welcome to CTI-110")

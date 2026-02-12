ca91c23d46ba67a5579fded3ba7db51541a7a8a9
JohansSolo <nurkkaj7276@student.faytechcc.edu>
2026-02-12 18:12:37 -0500

    Add files via upload
    Simple python script that calculates the power of an exponential expression and the total of a compound arithmetic expression

---
+++ P1HW1_Nurkka_Johans.py
@@ -0,0 +1,41 @@
+# Johans Nurkka
+# 11 February, 2026
+# P1HW1
+# Prints outcome of variable input to the power of a variable exponent
+
+print("-----Calculating Exponents-----")
+
+print()
+print()
+
+base_value = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+result = base_value ** exponent
+
+print()
+print()
+
+print(f"{base_value} raised to the power of {exponent} is {result}")
+
+print()
+print()
+
+print("-----Addition and Subtraction-----")
+
+print()
+print()
+
+start_int = int(input("Enter a starting integer: "))
+add_int = int(input("Enter an integer to add: "))
+sub_int = int(input("Enter an integer to subtract: "))
+sum_int=(start_int + add_int - sub_int)
+
+print()
+print()
+
+print(f"{start_int} + {add_int} - {sub_int} is equal to {sum_int}")
+
+print()
+print()
+
+input("Press Enter to Exit")
